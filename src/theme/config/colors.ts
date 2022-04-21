083845823b624e41ec4063067cca20d7dfa978f2
Alexis Guzman <alexis12989x@gmail.com>
2022-04-21 18:47:31 -0500

    feat(theme): create colors

---
+++ src/theme/config/colors.ts
@@ -0,0 +1,33 @@
+import { slate } from '@radix-ui/colors';
+
+const colors = {
+  ...slate,
+
+  appBackground: '$slate1',
+  subtleAppBackground: '$slate2',
+  uiElement: '$slate3',
+  hoveredUiElement: '$slate4',
+  activeUiElement: '$slate5',
+  subtleBorder: '$slate6',
+  normalBorder: '$slate7',
+  hoveredBorder: '$slate8',
+  solidBackground: '$slate9',
+  hoveredSolidBackground: '$slate10',
+  lowContrastText: '$slate11',
+  highContrastText: '$slate12',
+
+  primary1: '#FAFDFF',
+  primary2: '#F5FBFF',
+  primary3: '#EBF7FF',
+  primary4: '#E0F2FF',
+  primary5: '#CDEAFE',
+  primary6: '#A2D4F6',
+  primary7: '#80C5EF',
+  primary8: '#45B1ED',
+  primary9: '#0093E5',
+  primary10: '#0084D6',
+  primary11: '#006EC2',
+  primary12: '#002B4D',
+};
+
+export default colors;
