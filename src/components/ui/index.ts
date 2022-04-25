d08f002835150896b1a520128a3142922dd43a7a
Alexis Guzman <alexis12989x@gmail.com>
2022-04-25 13:50:08 -0500

    feat(ui): export text ui component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { default as A } from './A';
 export { default as Box } from './Box';
 export { default as Center } from './Center';
 export { default as Heading } from './Heading';
+export { default as Text } from './Text';
