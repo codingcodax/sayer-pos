7e2be8c1c612f63bc8ba08f920c7034f7f50aaf4
Alexis Guzman <alexis12989x@gmail.com>
2022-04-28 16:42:38 -0500

    feat(pages): setup index page with login component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import type { NextPage } from 'next';
+// import { useTheme } from 'next-themes';
+import Head from 'next/head';
+
+import { Center } from '~/components/ui';
+import { Login } from '~/components/containers';
 
 const Home: NextPage = () => {
+  // const { theme, resolvedTheme, setTheme } = useTheme();
+  //
+  // const toggleTheme = () =>
+  //   theme === 'dark' || resolvedTheme === 'dark'
+  //     ? setTheme('light')
+  //     : setTheme('dark');
+
   return (
-    <div>
-      <p>Home page</p>
-    </div>
+    <Center css={{ h: '$full', bg: '$subtleAppBackground' }}>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `if (document.cookie && document.cookie.includes('sayer-pos-auth')) { window.location.href = "/dashboard" }`,
+          }}
+        />
+      </Head>
+
+      <Login />
+    </Center>
   );
 };
 
