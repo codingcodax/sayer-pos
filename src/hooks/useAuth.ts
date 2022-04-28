64a06b3ac8f41dbb346d50045fa62c2d00e19c55
Alexis Guzman <alexis12989x@gmail.com>
2022-04-28 16:38:42 -0500

    feat: remove comments

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,13 +1,8 @@
-// import { useContext } from 'react';
 import cookie from 'js-cookie';
 
-// import { AuthContext } from '~/context/AuthContext';
 import supabase from '~/lib/supabaseClient';
-// import { SIGN_IN } from '~/store/actions/authActions';
 
 const useAuth = () => {
-  // const { authDispatch } = useContext(AuthContext);
-
   const signIn = async (email: string, password: string) => {
     try {
       const { error } = await supabase.auth.signIn({ email, password });
