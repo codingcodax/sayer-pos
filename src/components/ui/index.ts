82e243813ab3a324f03f126e1ac330bda3414cdb
Alexis Guzman <alexis12989x@gmail.com>
2022-04-28 16:40:55 -0500

    feat(theme): export input and button ui components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,8 @@
 export { default as A } from './A';
 export { default as Box } from './Box';
+export { default as Button } from './Button';
 export { default as Center } from './Center';
 export { default as Heading } from './Heading';
+export { default as Input } from './Input';
 export { default as Label } from './Label';
 export { default as Text } from './Text';
