08fa2e6e0903f51e79760c2f7b7059dced510874
Alexis Guzman <alexis12989x@gmail.com>
2022-04-28 12:45:22 -0500

    feat(ui): export label ui component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,4 +2,5 @@ export { default as A } from './A';
 export { default as Box } from './Box';
 export { default as Center } from './Center';
 export { default as Heading } from './Heading';
+export { default as Label } from './Label';
 export { default as Text } from './Text';
