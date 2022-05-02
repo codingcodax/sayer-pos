38c067dbcc714fbc6be8e57dab8d22d8423c7048
Alexis Guzman <alexis12989x@gmail.com>
2022-05-02 16:14:11 -0500

    feat(icons): add icons to logo small

--- src/components/containers/Nav.tsx
+++ src/components/containers/Nav.tsx
@@ -1,8 +1,24 @@
+import { Divider, Grid } from '~/components/ui';
+import {
+  ClockIcon,
+  HomeIcon,
+  LogoSmallIcon,
+  SettingsIcon,
+  ShopIcon,
+  WalletIcon,
+} from '~/components/icons';
+
 const Nav = () => {
   return (
-    <div>
-      <p>Nav component</p>
-    </div>
+    <Grid css={{ p: '$4', maxWidth: '100px' }}>
+      <LogoSmallIcon />
+      <Divider orientation='horizontal' />
+      <HomeIcon />
+      <ShopIcon />
+      <ClockIcon />
+      <WalletIcon />
+      <SettingsIcon />
+    </Grid>
   );
 };
 
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,6 @@
 export { default as ClockIcon } from './Clock';
 export { default as HomeIcon } from './Home';
-export { default as LogoSmall } from './LogoSmall';
+export { default as LogoSmallIcon } from './LogoSmall';
 export { default as SettingsIcon } from './Settings';
 export { default as ShopIcon } from './Shop';
 export { default as WalletIcon } from './Wallet';
