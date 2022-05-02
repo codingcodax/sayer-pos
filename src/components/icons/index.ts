6984475628e9cadd4fc121bdd1ad72d007932db6
Alexis Guzman <alexis12989x@gmail.com>
2022-05-02 16:02:36 -0500

    feat(icons): export wallet

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as ClockIcon } from './Clock';
 export { default as HomeIcon } from './Home';
 export { default as LogoSmall } from './LogoSmall';
 export { default as ShopIcon } from './Shop';
+export { default as WalletIcon } from './Wallet';
