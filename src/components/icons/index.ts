fc687995d074cf0b309e54db0e6570809340de19
Alexis Guzman <alexis12989x@gmail.com>
2022-05-03 10:20:14 -0500

    feat(icons): export logout component

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,7 @@
 export { default as ClockIcon } from './Clock';
 export { default as HomeIcon } from './Home';
 export { default as LogoSmallIcon } from './LogoSmall';
+export { default as LogoutIcon } from './Logout';
 export { default as SettingsIcon } from './Settings';
 export { default as ShopIcon } from './Shop';
 export { default as WalletIcon } from './Wallet';
