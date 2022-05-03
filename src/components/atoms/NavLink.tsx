1ba62ffee9ac89f77f965262dc548be475c8a6ac
Alexis Guzman <alexis12989x@gmail.com>
2022-05-03 10:19:01 -0500

    feat(atoms): setup nav link component

--- src/components/atoms/NavLink.tsx
+++ src/components/atoms/NavLink.tsx
@@ -1,8 +1,39 @@
-const NavLink = () => {
+import { FC, ReactNode } from 'react';
+
+import { A, Box, Text } from '../ui';
+
+import Link from './Link';
+
+interface NavLinkProps {
+  href: string;
+  name?: string;
+  children: ReactNode;
+}
+
+const NavLink: FC<NavLinkProps> = ({ href, name, children }) => {
   return (
-    <div>
-      <p>NavLink component</p>
-    </div>
+    <Box as='li'>
+      <Link href={href}>
+        <A
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            '&:hover>p': {
+              color: '$primary9',
+            },
+          }}
+          textDecoration='none'
+        >
+          {children}
+          {name && (
+            <Text css={{ fontSize: '$md', color: '$lowContrastText' }}>
+              {name}
+            </Text>
+          )}
+        </A>
+      </Link>
+    </Box>
   );
 };
 
