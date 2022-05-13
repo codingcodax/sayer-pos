783667f9160469dd73fb2f287f4d4371e3910fd6
Alexis Guzman <alexis12989x@gmail.com>
2022-05-13 15:37:19 -0500

    feat(api): create products api page

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -2,17 +2,49 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import prisma from '~/lib/prisma';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'GET')
-    return res.status(405).json({ msg: 'Method  not allowed' });
+const readProducts = async (res: NextApiResponse) => {
+  try {
+    const products = await prisma.product.findMany();
+
+    return res.status(200).json({ products, success: true });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: 'Error reading from database', success: true });
+  }
+};
+
+const addProducts = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name, description, slug, typeName, categoryName, lineName } =
+    req.body;
 
   try {
-    const data = await prisma.inquiry.findMany({});
-    return res.status(200).json({ data });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ msg: 'Something went wrong' });
+    const newProduct = await prisma.product.create({
+      data: {
+        name,
+        description,
+        slug,
+        typeName,
+        categoryName,
+        lineName,
+      },
+    });
+
+    return res.status(200).json({ newProduct, success: true });
+  } catch (error) {
+    console.error('Request error', error);
+    res.status(500).json({ error: 'Error adding book', success: false });
   }
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  if (method === 'GET') return await readProducts(res);
+  if (method === 'POST') return await addProducts(req, res);
+
+  res.status(500).json({ error: 'Method not allowed', success: false });
 };
 
 export default handler;
