9a4bd568670d4fb5391c6af82becbf84922f181a
Alexis Guzman <alexis12989x@gmail.com>
2022-05-13 19:04:37 -0500

    feat(pages): import choose category component

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,22 @@
-const Products = () => {
+import { GetStaticProps } from 'next';
+
+import prisma from '~/lib/prisma';
+import { ChooseCategory } from '~/components/containers';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const categories = await prisma.category.findMany();
+
+  return { props: { categories } };
+};
+
+const Products = ({ categories }) => {
+  console.log(categories);
+
   return (
     <div>
-      <p>Products page</p>
+      <ChooseCategory />
+      <p>Products</p>
+      <p>Basquet</p>
     </div>
   );
 };
