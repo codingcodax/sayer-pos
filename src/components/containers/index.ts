e359ea94bae47d6b79874154ed966a0c0b6445b5
Alexis Guzman <alexis12989x@gmail.com>
2022-05-17 16:57:14 -0500

    feat(containers): export products components

--- src/components/containers/index.ts
+++ src/components/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as ChooseType } from './ChooseType';
 export { default as Login } from './Login';
 export { default as Nav } from './Nav';
+export { default as Products } from './Products';
