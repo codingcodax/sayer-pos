09473ebde7701d37bf5da58b6f586d868f4e50c2
Alexis Guzman <alexis12989x@gmail.com>
2022-05-19 17:45:18 -0500

    feat(containers): setup products components

--- src/components/containers/Products.tsx
+++ src/components/containers/Products.tsx
@@ -1,9 +1,36 @@
-import { Box, Text } from '../ui';
+import { FC } from 'react';
 
-const Products = () => {
+import { Product as ProductType } from '~/@types';
+import { Product } from '~/components/atoms';
+import { Box, Text, Heading, Grid } from '~/components/ui';
+
+interface ProductsProps {
+  products: ProductType[];
+  type: string | string[];
+}
+
+const Products: FC<ProductsProps> = ({ products, type }) => {
   return (
     <Box>
-      <Text>Products Component</Text>
+      <Box>
+        <Heading as='h2' css={{ textTransform: 'capitalize' }}>
+          {type}
+        </Heading>
+        <Text>{products.length}</Text>
+      </Box>
+
+      <Grid css={{ gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '$4' }}>
+        {products.map(({ id, name, medias }) => (
+          <Product
+            key={id}
+            // @ts-ignore
+            alt={medias[0].media.altText}
+            name={name}
+            // @ts-ignore
+            url={medias[0].media.url}
+          />
+        ))}
+      </Grid>
     </Box>
   );
 };
