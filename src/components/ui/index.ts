b71f85d9897773ae5eeabe95b1407b61e0f65caa
Alexis Guzman <alexis12989x@gmail.com>
2022-05-20 10:50:12 -0500

    feat(ui): export flex component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,7 @@ export { default as Box } from './Box';
 export { default as Button } from './Button';
 export { default as Center } from './Center';
 export { default as Divider } from './Divider';
+export { default as Flex } from './Flex';
 export { default as Grid } from './Grid';
 export { default as Heading } from './Heading';
 export { default as Input } from './Input';
