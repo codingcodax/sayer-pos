c6ea653c0918def7c4c56f5186f34abee93d214d
Alexis Guzman <alexis12989x@gmail.com>
2022-05-24 13:40:30 -0500

    feat(containers): create product to add component

---
+++ src/components/containers/ProductToAdd.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from '~/components/ui';
+
+const ProductToAdd = () => {
+  return (
+    <Box css={{ bg: '$appBackground' }}>
+      <Text>Product To Add component</Text>
+    </Box>
+  );
+};
+
+export default ProductToAdd;
