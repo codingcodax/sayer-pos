5a7bbadd9d34d1c88462ffa467e518b3318214b1
Alexis Guzman <alexis12989x@gmail.com>
2022-05-24 10:48:21 -0500

    feat(containers): import product component

--- src/components/containers/Poducts.tsx
+++ src/components/containers/Poducts.tsx
@@ -11,15 +11,8 @@ interface ProductsProps {
 const Products: FC<ProductsProps> = ({ products }) => {
   return (
     <Grid css={{ gridTemplateColumns: '1fr 1fr 1fr', gap: '$4' }}>
-      {products.map(({ id, name, medias }) => (
-        <Product
-          key={id}
-          // @ts-ignore
-          alt={medias[0].media.altText}
-          name={name}
-          // @ts-ignore
-          url={medias[0].media.url}
-        />
+      {products.map((product) => (
+        <Product key={product.id} {...product} />
       ))}
     </Grid>
   );
