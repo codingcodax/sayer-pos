a8fa1f7ebd9b2d09c7592471588864e7bb5d16c6
Alexis Guzman <alexis12989x@gmail.com>
2022-05-25 17:20:50 -0500

    feat(atoms): export option component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,5 @@
 export { default as Link } from './Link';
 export { default as NavLink } from './NavLink';
+export { default as Option } from './Option';
 export { default as Product } from './Product';
 export { default as Type } from './Type';
